d85c484766c6f7cd0b45d33abf77bc57d44627d9
thisisdilmurod <dilmurod.abdusamadov2004@gmail.com>
2024-01-30 23:21:24 +0500

    Add operation available

---
+++ FunPro.CW2.19248.DAL/ClashManager.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FunPro.CW2._19248.DAL
+{
+    internal class ClashManager
+    {
+
+    }
+}
---
+++ FunPro.CW2.19248.DAL/PlayerManager.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FunPro.CW2._19248.DAL
+{
+    internal class PlayerManager
+    {
+        public void Create(Player player)
+        {
+            using var conn = new SQLiteConnection();
+            try
+            {
+                var sql = $"INSERT INTO " +
+                    $"Player (Name, IsPvpEnabled, LastGameDate, SCore) " +
+                    $"VALUES ({player.Name}, {player.IsPvpEnabled}, {player.LastGameDate}, {player.Score})";
+                using var command = new SQLiteCommand(sql, conn);
+                conn.Open();
+                command.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                if (conn.State != ConnectionState.Closed)
+                {
+                    conn.Close();
+                }
+            }
+        };
+        public void Update(Player player);
+        public void Delete(int id);
+        public List<Player> GetAll();
+
+        // TODO: Deleting player records, sort by name, filter by score
+        // TODO: Import System.Data and System.Data.SQLite
+        // TODO: Check input type!!!
+    }
+}
